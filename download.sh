204270a64bfae0be2b9429997f2aab21aa06d665
robert1003 <b07902047@ntu.edu.tw>
2020-04-15 20:03:11 +0800

    early

---
+++ download.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+early=140.112.90.197:9763/hw2/early.zip
+final=140.112.90.197:9763/hw2/final.zip
+
+wget "${early}" -O ./temp.zip
+unzip temp.zip
+rm temp.zip
+
+#wget "${final}" -O ./temp.zip
+#unzip temp.zip
+#rm temp.zip
---
+++ early.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python3.7 final/eval.py $1 $2
