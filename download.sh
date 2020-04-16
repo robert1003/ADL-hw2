f2105eefe62182707c6a9413331f5d26a847f588
robert1003 <b07902047@ntu.edu.tw>
2020-04-16 09:12:08 +0800

    Fix download

--- download.sh
+++ download.sh
@@ -3,10 +3,10 @@
 early=140.112.90.197:9763/hw2/early.zip
 final=140.112.90.197:9763/hw2/final.zip
 
-wget "${early}" -O ./temp.zip
-unzip temp.zip
-rm temp.zip
-
-#wget "${final}" -O ./temp.zip
+#wget "${early}" -O ./temp.zip
 #unzip temp.zip
 #rm temp.zip
+
+wget "${final}" -O ./temp.zip
+unzip temp.zip
+rm temp.zip
